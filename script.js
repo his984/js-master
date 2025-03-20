94a22b1a5d1fa9ea3d844739c9a466ebd5836505
his984 <his984@gmail.com>
2025-03-20 02:25:55 +0100

    Functions - Default Parameters

--- script.js
+++ script.js
@@ -1,41 +1,22 @@
 "use strict";
 
-const cla = ["a", "b", "c"];
-const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-const obj = {
-  name: "John",
-  age: 30,
-  // cla: cla,
-  // es6 enhanced object literals syntax
-  cla,
-  // method
-  greet() {
-    console.log("Hello");
-  },
-  // Computed property names
-  [weekdays[3]]: "Thursday",
-  // Short hand syntax
-  weekdays, // weekdays: weekdays
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1; // default value
+  // price = price || 199; // default value
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
 };
-console.log(obj);
-// Optional chaining operator (?.)
-
-const obj2 = {
-  name: "John",
-  age: 30,
-  cla: cla,
-  greet() {
-    console.log("Hello");
-  },
-  weekdays: weekdays,
-  address: {
-    street: "123 Main St",
-    city: "New York",
-    country: "USA",
-  },
-};
-console.log(obj2.address?.country);
-
-
-
-
+createBooking("LH123", 3, 999);
+createBooking("LH123", 2);
