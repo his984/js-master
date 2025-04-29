5cfb04f7f02b56ea5e9334a0006306318ec59f07
his984 <his984@gmail.com>
2025-04-29 23:52:40 +0200

    Transfer money

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 "use strict";
 // BANKIST APP
-// Data
-const account1 = {
-  owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-const account2 = {
-  owner: "Jessica Davis",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-const account3 = {
-  owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-const account4 = {
-  owner: "Sarah Smith",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-const accounts = [account1, account2, account3, account4];
-// ELEMENTS
+//  Data
+const accounts = [
+  {
+    owner: "Jonas Schmedtmann",
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2,
+    pin: 1111,
+  },
+  {
+    owner: "Jessica Davis",
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+  },
+  {
+    owner: "Steven Thomas Williams",
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+  },
+  {
+    owner: "Sarah Smith",
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+  },
+];
+
+// #region Elements
 const containerMovements = document.querySelector(".movements");
-const labelBalance = document.querySelector(".balance_value");
+const sumBalance = document.querySelector(".balance_value");
 const sumIn = document.querySelector(".summary__value--in");
 const sumOut = document.querySelector(".summary__value--out");
 const interestValue = document.querySelector(".summary__value--interest");
@@ -36,24 +38,28 @@ const inputUser = document.querySelector(".inputUser");
 const inputPin = document.querySelector(".inputPin");
 const loginBtn = document.querySelector(".loginBtn");
 const welcomeMessage = document.querySelector(".welcome");
-const content = document.querySelector(".content");
+const app = document.querySelector(".app");
+const formInputTo = document.querySelector(".transferTo");
+const formInputAmount = document.querySelector(".amount");
+const btnTransform = document.querySelector(".transferBtn");
+// #endregion
+
 // DisplayMovements
 const displayMovements = function (movements) {
   containerMovements.innerHTML = "";
-  movements.forEach(function (mov, i) {
+  movements.forEach(function (mov) {
     const type = mov > 0 ? "deposit" : "withdraw";
     const html = `<div class="movements_row">
                     <div class="movements_details">
-                        <div class="movements__type ${type}">${type} ${i + 1} 
+                      <div class="movements__type ${type}">${type}</div>
+                      <div>2025-01-01</div>
                     </div>
-                    <div class="movements_Start at">2025-01-01 - 00:00</div>
-                    </div>
-                    <div class="movements_value">${mov}$</div>
+                    <div class="movements_value">${mov}</div>
                   </div>`;
     containerMovements.insertAdjacentHTML("afterbegin", html);
   });
 };
-
+// -------------------------------------------------------
 // Computing usernames
 const createUserNames = function (accs) {
   accs.forEach(function (acc) {
@@ -65,40 +71,13 @@ const createUserNames = function (accs) {
   });
 };
 createUserNames(accounts);
-
-// Event handler
-let currentAccount;
-loginBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  currentAccount = accounts.find((acc) => acc.userName === inputUser.value);
-  // console.log(currentAccount);
-  if (currentAccount?.pin === Number(inputPin.value)) {
-    //  Display UI and message
-    welcomeMessage.textContent = `Welcome back, ${
-      currentAccount.owner.split(" ")[0]
-    }`;
-    content.style.opacity = 1;
-    // Clear input fields
-    inputPin.value = "";
-    inputPin.blur();
-    inputUser.value = "";
-
-    // Display movements
-    displayMovements(currentAccount.movements);
-    // Display balance
-    calcDisplayBalance(currentAccount.movements);
-    // Display summary
-    calcDisplaySummary(currentAccount);
-  }
-});
-
+// -------------------------------------------------------
 // calcDisplayBalance
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance}€
-`;
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  sumBalance.textContent = `${acc.balance}€`;
 };
-
+// -------------------------------------------------------
 // calcDisplaySummary
 const calcDisplaySummary = function (acc) {
   // IN
@@ -122,3 +101,60 @@ const calcDisplaySummary = function (acc) {
   interestValue.textContent = `${interest}€`;
   interestValue.style.color = "green";
 };
+// -------------------------------------------------------
+// Update UI
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc.movements);
+  // Display balance
+  calcDisplayBalance(acc);
+  //  Display summary
+  calcDisplaySummary(acc);
+};
+// -------------------------------------------------------
+
+// Event handler (Login)
+let currentAccount;
+loginBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  currentAccount = accounts.find(function (acc) {
+    return acc.userName === inputUser.value;
+  });
+  if (currentAccount?.pin === Number(inputPin.value)) {
+    //  Display UI and message
+    welcomeMessage.textContent = `Welcome, ${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    // Clear input fields
+    inputPin.value = "";
+    inputPin.blur();
+    inputUser.value = "";
+    // Display account details
+    app.style.opacity = 100;
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+// -------------------------------------------------------
+// Transfer Money
+btnTransform.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = Number(formInputAmount.value);
+  const receiverAccount = accounts.find(
+    (acc) => acc.userName === formInputTo.value
+  );
+  // console.log(amount, receiverAccount);
+  formInputAmount.value = formInputTo.value = "";
+  if (
+    amount > 0 &&
+    receiverAccount &&
+    currentAccount.balance >= amount &&
+    receiverAccount?.userName !== currentAccount.userName
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAccount.movements.push(amount);
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
